e13e95b665bad1bf7e93087109c4a857d9541b3b
BaptisteLeBescond_Jussieu <baptiste.lb@gmail.com>
2015-12-01 15:29:35 +0100

    Add Constructor and Getters in Class Pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -5,4 +5,56 @@ public class Pokemon {
 	private String Name;
 	private int Stage;
 	private Pokemon EvolveFrom;
+	
+	/**
+	 * 
+	 * @param t
+	 * @param lp
+	 * @param n
+	 * @param s
+	 */
+	public Pokemon(String t, int lp, String n, int s){
+		Type = t;
+		LifePoints = lp;
+		Name = n;
+		Stage = s;
+		EvolveFrom = null;
+	}
+	
+	/**
+	 * 
+	 * @param t
+	 * @param lp
+	 * @param n
+	 * @param s
+	 * @param ef
+	 */
+	public Pokemon(String t, int lp, String n, int s, Pokemon ef){
+		Type = t;
+		LifePoints = lp;
+		Name = n;
+		Stage = s;
+		EvolveFrom = ef;
+	}
+	
+	public String getType() {
+		return Type;
+	}
+	
+	public int getLifePoints() {
+		return LifePoints;
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	
+	public int getStage() {
+		return Stage;
+	}
+	
+	public Pokemon getEvolveFrom() {
+		return EvolveFrom;
+	}
+
 }
